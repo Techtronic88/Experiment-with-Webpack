066af39e533561b72ad21bf87d8c05323ba0d0d7
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-06-18 14:45:40 +1000

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
+const path = require("path")
+
+// the entry object has dynamic names like
+// [ hash ]
+// [ chunkhash ]
+// [ name ]
+// [ id ]
+// [ query ]
+// [ contenthash ]
+
 module.exports = {
-    mode: "development"
-    // This is actually the configuration webpack uses
-    // How we webpack knows it has to use this configurations ?
-}
+    mode: "development",
+    entry: {
+        myfile: "./src/engine.js"
+    },
+    output: {    
+        path: path.resolve(__dirname, "build"), 
+        filename: "js/[name].js",
+        publicPath: "/assets/",
+        libraryTarget:"var",
+        library: "myfirstlibrary"
+
+    }
+} 
