a918b3de52ad05155273da7d3d1429f9a5a7bc91
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-06-21 00:08:39 +1000

    Add files via upload
    - Changes made to this commit 
    - Installed "clean-webpack-plugin"
    - Configure webpack.common.js to produce 2 different entries point from index.js to vendor.js

--- 2ndWEBPACK-PROJECT/src/index.js
+++ 2ndWEBPACK-PROJECT/src/index.js
@@ -3,5 +3,7 @@ import "./main.scss"
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
+console.log("HI");
 const componentService = new ComponentService();
 run(alertService, componentService);
+console.log("Hi");
